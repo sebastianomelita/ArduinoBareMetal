8d20644d55d0ac639f708aaef1a14031becdfae9
Sebastiano Melita <sebastiano.melita@gmail.com>
2023-11-12 19:43:57 +0100

    Add files via upload

---
+++ urutils.h
@@ -0,0 +1,38 @@
+#ifndef __URUTILS_H__
+#define __URUTILS_H__
+
+void waitUntilInputLow(int btn, unsigned t)
+{
+    while(!digitalRead(btn)==LOW){
+	    delay(t);
+    }
+}
+
+typedef struct 
+{
+	unsigned long elapsed, last;
+	bool timerState=false;
+	void reset(){
+		elapsed = 0;
+		last = millis();
+	}
+	void stop(){
+		timerState = false;
+    		elapsed += millis() - last;
+	}
+	void start(){
+		timerState = true;
+		last = millis();
+	}
+	unsigned long get(){
+		if(timerState){
+			return millis() - last + elapsed;
+		}
+		return elapsed;
+	}
+	void set(unsigned long e){
+		reset();
+		elapsed = e;
+	}
+} DiffTimer;
+#endif
